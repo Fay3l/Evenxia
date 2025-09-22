825de455b91c4e874f3553048406e5780e4c1603
Fayel MOHAMED <mohamed.fayel@yahoo.com>
2025-09-22 23:37:38 +0200

    design

--- my-app-evenxia/src/components/Card.tsx
+++ my-app-evenxia/src/components/Card.tsx
@@ -29,9 +29,18 @@ export default function Card({ title = "", description = "", imageUrl = "", addr
                         <p className="font-bold">{start_date} - {end_date}</p>
                         <button onClick={onButtonClick} className="btn btn-primary max-sm:btn-sm p-4">Voir plus en détails</button>
                     </div>
-                    <div className="max-sm:text-sm">
-                        <p className="italic">{address}</p>
+                    <div className="flex flex-row justify-between items-center w-full max-sm:flex-col gap-2">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                        </div>
+                        <div className="max-sm:text-sm">
+                            <p className="italic">{address}</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>)
